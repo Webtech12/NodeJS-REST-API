f6afed48351dc2f4f8ffa28b8e3b4f32dafcc308
Sherwin <sherwinlukes@gmail.com>
2020-02-17 12:30:16 +0500

    securing passwords 2

--- src/models/user.js
+++ src/models/user.js
@@ -31,9 +31,8 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
     
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 8)
-    }
+    if (this.isModified('password')) this.password = await bcrypt.hash(this.password, 8) 
+
     next()
   })
 
